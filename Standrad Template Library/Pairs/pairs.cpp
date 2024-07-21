a8eb09f5499998f94bb65eea8204e0741e61f098
developmentwithaditya <developmentwithaditya@gmail.com>
2024-07-21 10:09:03 +0530

    explaining pairs

---
+++ Standrad Template Library/Pairs/pairs.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <utility>
+using namespace std;
+
+void explainPair();
+
+int main()
+{
+	explainPair();
+
+	return 0;
+}
+
+void explainPair()
+{
+	pair<int, int> pairIs = {1, 2};
+	cout << pairIs.first << " " << pairIs.second << endl;
+
+	// Nested pairs
+
+	pair<int, pair<int, int>> nestedPairIs = {1, {2, 3}};
+	cout << nestedPairIs.first << " " << nestedPairIs.second.first << " " << nestedPairIs.second.second << endl;
+
+	// Pairs of array
+
+	pair<int, int> arr[] = {{1, 2}, {3, 4}, {5, 6}};
+	cout << arr[0].first << endl;
+}
